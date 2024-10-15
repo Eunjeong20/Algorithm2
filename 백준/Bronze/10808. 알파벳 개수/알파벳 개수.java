666f70554cbae89bf526170de26bfc668e22f039
최은정 <102710280+Eunjeong20@users.noreply.github.com>
2024-10-15 14:39:21 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 184 ms, Memory: 18292 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    Scanner sc = new Scanner(System.in);
+	    String str = sc.next();
+	    int[] alpha = new int[26];
+        for (int i=0; i<str.length(); i++) {
+            alpha[str.charAt(i)-'a']++;
+        }
+        for (int i=0; i<26; i++) {
+            System.out.print(alpha[i]+" ");
+        }
+	}
+}
