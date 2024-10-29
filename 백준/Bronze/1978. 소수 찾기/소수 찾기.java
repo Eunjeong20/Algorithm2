2c49aa64d82a916cc37ae6d0e2aadd69e3dbf118
최은정 <102710280+Eunjeong20@users.noreply.github.com>
2024-10-29 12:08:34 +0900

    [Bronze II] Title: 소수 찾기, Time: 184 ms, Memory: 17812 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+
 		Scanner scan = new Scanner(System.in);
         int tc = scan.nextInt();
         int cnt = 0;
@@ -10,17 +11,19 @@ public class Main {
             boolean is_prime = true;
             int num = scan.nextInt();
             
-            if (num == 1) is_prime = false;
-            else if (num > 2) {
-                for(int j=2; j<=(num/2); j++) {
-                    if (num%j==0) {
-                        is_prime = false;
-                        break;
-                    }
+            if (num == 1) {
+            	is_prime = false;
+            	continue;
+            }
+            for(int j=2; j<=(num/2); j++) {
+            	if (num%j==0) {
+                	is_prime = false;
+                	break;
                 }
             }
             if (is_prime == true) cnt++;
 		}
 		System.out.println(cnt);
-	} 
+		
+	}
 }
